19a911b3d2ba72f31d0407fb263c8cc9728ffcb2
Ninosaur <110027245+Syncony@users.noreply.github.com>
2025-03-11 18:36:55 +0800

    Update sing-box.sh

--- sing-box.sh
+++ sing-box.sh
@@ -169,7 +169,7 @@ read -p "自定义ShortID(不需要就直接回车):" SID
 [ -z ${SID} ] && SID=`openssl rand -hex 8`
 
 read -p "自定义SNI(不需要就直接回车):" SNI
-[ -z ${SNI} ] && SNI="jetpack.com"
+[ -z ${SNI} ] && SNI="www.goto.com"
 
 read -p "自定义私钥(不需要就直接回车):" PIK
 if [[ -z ${PIK} ]];then
